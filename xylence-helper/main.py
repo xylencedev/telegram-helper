def553f370e3019becb8a4902de387d42974b70f
xylencedev <xylencedev@gmail.com>
2026-01-01 21:28:19 +0700

    Update main.py

--- xylence-helper/main.py
+++ xylence-helper/main.py
@@ -7,7 +7,7 @@ import re
 from urllib.parse import urlparse
 from collections import defaultdict
 import json
-from pyromod import listen
+
 import requests
 import pyrogram.utils
 
@@ -1062,5 +1062,6 @@ async def on_cancel(client, callback_query):
 if __name__ == "__main__":
     print("Starting Xylence-Helper bot...")
     app.run()
+
 
 
