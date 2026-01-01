2a18308a12b2259b6db238290640f9c73f65a82c
xylencedev <xylencedev@gmail.com>
2026-01-01 21:26:41 +0700

    Update main.py

--- xylence-helper/main.py
+++ xylence-helper/main.py
@@ -1,3 +1,14 @@
+
+
+import logging
+import asyncio
+from functools import partial
+import re
+from urllib.parse import urlparse
+from collections import defaultdict
+import json
+from pyromod import listen
+import requests
 import pyrogram.utils
 
 def fixed_get_peer_type(peer_id: int) -> str:
@@ -8,22 +19,12 @@ def fixed_get_peer_type(peer_id: int) -> str:
         return "channel"
     else:
         return "chat"
-
-pyrogram.utils.get_peer_type = fixed_get_peer_type
-
-import logging
-import asyncio
-from functools import partial
-import re
-from urllib.parse import urlparse
-from collections import defaultdict
-import json
-
-import requests
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto, InputMediaVideo
-from pyromod import listen
 
+
+
+pyrogram.utils.get_peer_type = fixed_get_peer_type
 
 
 API_ID = 21001953
@@ -1061,4 +1062,5 @@ async def on_cancel(client, callback_query):
 if __name__ == "__main__":
     print("Starting Xylence-Helper bot...")
     app.run()
+
 
